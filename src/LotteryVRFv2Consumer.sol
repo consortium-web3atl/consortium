673d5c5949588bb53088bb1fc93905be78d3fd11
Gabriel Fior <fior.gbm@gmail.com>
2023-11-03 18:56:03 -0300

    Chainlink compiling

--- src/LotteryVRFv2Consumer.sol
+++ src/LotteryVRFv2Consumer.sol
@@ -6,9 +6,9 @@
 pragma solidity ^0.8.7;
 
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import "@chainlink/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/v0.8/shared/access/ConfirmedOwner.sol";
 
 contract LotteryVRFv2Consumer is VRFConsumerBaseV2, ConfirmedOwner {
 
